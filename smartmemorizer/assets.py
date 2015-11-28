169621bb0339b74cf2cdcf6518164c3acfdad279
jack.yun <jack.yun@recobell.com>
2015-11-28 22:36:12 +0900

    fix : fit jquery lib path

--- smartmemorizer/assets.py
+++ smartmemorizer/assets.py
@@ -10,7 +10,7 @@ css = Bundle(
 )
 
 js = Bundle(
-    'libs/jQuery/dist/jquery.js',
+    'libs/jquery/dist/jquery.js',
     'libs/bootstrap/dist/js/bootstrap.js',
     'libs/datatables.net/js/jquery.dataTables.min.js',
     'js/plugins.js',
